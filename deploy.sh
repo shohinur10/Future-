5a9785477fa51a158c8d4a6cef6ead5764afb63f
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-09-07 18:58:10 +0900

    feat: deploy via shell & pm2

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#Prodcution 
+# git reset --hard
+# git checkout master 
+# git pull origin master 
+
+
+npm i yarn -g
+yarn global add serve
+yarn 
+yarn run build  
+pm2 start "yarn run start:prod" --name "future-furniture"
+pm2 save
